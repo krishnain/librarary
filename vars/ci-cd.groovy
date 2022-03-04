ad12cd90d62476bde8d1f15a679bda281626f36b
krishnain <86010658+krishnain@users.noreply.github.com>
2022-03-04 07:19:45 +0530

    Create ci-cd.groovy

---
+++ vars/ci-cd.groovy
@@ -0,0 +1,20 @@
+def newGit(repo)
+{
+  git  "${repo}"
+}
+
+def newMaven()
+{
+    sh 'mvn package' 
+}
+def newDeploy(ip,appname)
+{
+  deploy adapters: [tomcat9(credentialsId: 'bea82394-b01c-4339-9304-b5452c8e1503', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
+}
+
+
+
+
+
+
+
